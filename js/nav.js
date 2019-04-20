fad6dc206b13d997f4509cf8a2191ec31fd79ff5
Mateusz Szczepaniak <szczepaniakmateusz1989@gmail.com>
2019-04-20 11:56:35 +0200

    menu-top

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function nav() {
+    var x = document.getElementById("nav-hidden");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
